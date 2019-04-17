b8bfde2cd74b288a40a04405dc006d7508ee6bf9
Nate Willis <natewillis1@gmail.com>
2019-04-16 19:33:04 -0500

    added the room posisition surrounding grid code

--- prototype.roomposition.js
+++ prototype.roomposition.js
@@ -1,5 +1,32 @@
-// create a new function for StructureTower
+// Create a shorthand function
 RoomPosition.prototype.shorthand =
     function () {
         return this.roomName+'X'+this.x+'Y'+this.y
+    };
+
+// Create a function that returns array of room positions around a single position
+RoomPosition.prototype.surround_grid =
+    function () {
+
+        // Setup return array
+        retArr = [];
+
+        // Setup Bounds
+        let minX = Math.max(this.x-1,0);
+        let maxX = Math.min(this.x+1,49);
+        let minY = Math.max(this.y-1,0);
+        let maxY = Math.min(this.y+1,49);
+
+        // Create Grid
+        for (let i = minX; i <= maxX; i++) {
+            for (let j = minY; j <= maxY; j++) {
+                if (i !== this.x && j !== this.y) {
+                    retArr.push(new RoomPosition(i,j,this.RoomPosition));
+                }
+            }
+        }
+
+        // Return it
+        return retArr;
+
     };
