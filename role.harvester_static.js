51ae0d2bde4c88fd93261bf417503de7ca3342e8
Nate Willis <natewillis1@gmail.com>
2019-04-19 21:15:23 -0500

    finished fixing the static harvester

--- role.harvester_static.js
+++ role.harvester_static.js
@@ -20,7 +20,9 @@ var roleHarvester = {
                 creep.memory.harvest.source_id = source_obj.id;
                 creep.memory.harvest.harvest_pos_shorthand = source_obj.harvest_pos_shorthand;
                 creep.memory.harvest.harvesting = true;
-                creep.memory.harvest.container_id = Memory.empire.rooms[creep.memory.room].objects_at_position.containers;
+                creep.memory.harvest.container_id = '';
+                creep.memory.harvest.construction_site_id = '';
+                creep.memory.harvest.container_build_state = 'harvest';
             }
 
         }
@@ -29,69 +31,62 @@ var roleHarvester = {
         let my_source = Game.getObjectById(creep.memory.harvest.source_id);
         let harvest_location = utilities.room_position_from_shorthand(creep.memory.harvest.harvest_pos_shorthand)
 
-        // Get container
-        creep.memory.harvest.container_id = Memory.empire.rooms[creep.memory.room].objects_at_position.containers;
-        if (creep.memory.harvest.container_id == '') {
-            creep.memory.harvest.container_id = Memory.empire.rooms[creep.memory.room].objects_at_position.containers;
+        // Update container from empire memory
+        if (harvest_location.shorthand() in Memory.empire.rooms[creep.memory.room].objects_at_position) {
+            creep.memory.harvest.container_id = Memory.empire.rooms[creep.memory.room].objects_at_position[harvest_location.shorthand()].containers;
+        } else {
+            creep.memory.harvest.container_id = '';
         }
-
-        // Figure out if we still need to build a container
-        if (creep.memory.harvest.container_id == '') {
-            let look_return = harvest_location.lookFor(
-                LOOK_STRUCTURES
-            )
-            look_return = look_return.filter(function (structure) {
-                return structure.structureType == 'container';
-            });
-            if (look_return.length>0) {
-                creep.memory.container_id = look_return[0].id;
-            }
-        }
+        
 
         // Check if we're at the harvest location
-        if (!utilities.compare_RoomPositions(harvest_location,creep.pos)) {
+        if (!harvest_location.isEqualTo(creep.pos)) {
             // Move To The Harvest Location
             creep.moveTo(harvest_location, {visualizePathStyle: {stroke: '#ffaa00'},reusePath: 10});
         } else {
             // Were where we need to be
-            if (creep.memory.container_id == '') {
-                console.log('container needs to be created!');
-                let look_return = harvest_location.lookFor(LOOK_CONSTRUCTION_SITES);
-                if (look_return.length == 0) {
-                    // Create Construction Site For Container
-                    if (Game.rooms[harvest_location.roomName].createConstructionSite(harvest_location,STRUCTURE_CONTAINER) !== OK) {
-                        console.log(creep.name + ' couldnt build his construction site')
-                        return;
-                    } else {
-                        look_return = harvest_location.lookFor(LOOK_CONSTRUCTION_SITES);
-                    }
-                }
-                let my_construction_site = look_return[0];
+            if (creep.memory.harvest.container_id == '') {
 
-                // Init Memory For The First Time
-                if (typeof creep.memory.harvest_container_build_state  == 'undefined') {
-                    creep.memory.harvest_container_build_state = 'harvest';
+                console.log('container needs to be created!');
+                let my_construction_site = null;
+                if (creep.memory.harvest.construction_site_id !== '') {
+                    my_construction_site = Game.getObjectById(creep.memory.harvest.construction_site_id);
+                } else {
+                    let look_return = harvest_location.lookFor(LOOK_CONSTRUCTION_SITES);
+                    if (look_return.length == 0) {
+                        // Create Construction Site For Container
+                        if (Game.rooms[harvest_location.roomName].createConstructionSite(harvest_location,STRUCTURE_CONTAINER) !== OK) {
+                            console.log(creep.name + ' couldnt build his construction site')
+                            return;
+                        } else {
+                            look_return = harvest_location.lookFor(LOOK_CONSTRUCTION_SITES);
+                        }
+                    }
+                    my_construction_site = look_return[0];
+                    creep.memory.harvest.construction_site_id = my_construction_site.id;
                 }
+                
 
                 // Check For State Change
                 if (creep.carry.energy == creep.carryCapacity) {
-                    creep.memory.harvest_container_build_state = 'build';
+                    creep.memory.harvest.container_build_state = 'build';
                 } else if (creep.carry.energy == 0) {
-                    creep.memory.harvest_container_build_state = 'harvest';
+                    creep.memory.harvest.container_build_state = 'harvest';
                 }
 
                 // Perform action based on state
-                if (creep.memory.harvest_container_build_state == 'harvest') {
+                if (creep.memory.harvest.container_build_state == 'harvest') {
                     creep.harvest(my_source);
                 } else {
                     let retval = creep.build(my_construction_site);
                 }
 
                 return;
+                
             } else { // We have a container and we're at the source, lets harvest!
 
                 // Get Container
-                let my_container = Game.getObjectById(creep.memory.container_id);
+                let my_container = Game.getObjectById(creep.memory.harvest.container_id);
 
                 // Check Container For Damage
                 if (my_container.hits < my_container.hitsMax && creep.carry.energy == creep.carryCapacity) {
